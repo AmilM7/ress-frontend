a701ba9e3145cc257be0b0338af036a62a16e8ba
amars21 <amar.sose@hotmail.com>
2022-04-09 01:45:04 +0200

    Header for logged user added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header-welcome/header.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MainComponent } from './main/main.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HeaderUserComponent } from './header-user/header-user.component';
+import {MatIconModule} from "@angular/material/icon";
+import { FavoriteComponent } from './favorite/favorite.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
@@ -22,11 +28,17 @@ import { AboutUsComponent } from './about-us/about-us.component';
     SignUpComponent,
     PageNotFoundComponent,
     MainComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    HeaderUserComponent,
+    FavoriteComponent,
+    ProfileComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,9 @@
 export class Routex {
   public static Empty = '';
-  public static signUp= 'signup';
+  public static signUp = 'signup';
   public static logIn = 'logIn';
-  public static aboutUs='about';
+  public static aboutUs = 'about';
+  public static profile = 'profile';
+  public static favorite= 'favorite';
+  public static logout= 'logout';
 }
