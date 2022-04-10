5a95c14882d0653d1972ecb8668e67df38a5ffb9
amars21 <amar.sose@hotmail.com>
2022-04-10 16:07:25 +0200

    Added header and footer, removed unnecessary files.

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,8 +1,8 @@
 export class Routex {
   public static Empty = '';
   public static signUp = 'signup';
-  public static logIn = 'logIn';
-  public static aboutUs = 'about';
+  public static logIn = 'login';
+  public static aboutUs = 'aboutus';
   public static profile = 'profile';
   public static favorite= 'favorite';
   public static logout= 'logout';
