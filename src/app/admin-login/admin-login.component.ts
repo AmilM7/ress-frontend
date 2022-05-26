89691bbe76a3784fbee910ea187472856cb58997
marslanagic <63009940+marslanagic@users.noreply.github.com>
2022-05-26 16:22:58 +0200

    Added: login sreen for admin; Update: routing for the component

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Person} from "../models/person";
 
 @Component({
   selector: 'app-admin-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent implements OnInit {
+  public form!: FormGroup;
+  person: Person | undefined;
 
-  constructor() { }
+  constructor(private formBuilder : FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      password: [this.person?.password || '', [Validators.required]]
+    })
   }
 
+
+
+  get password(){
+    return this.form.get('password')
+  }
+
+  submit(): void {
+    console.log(this.form.value.password);
+    this.form.reset();
+  }
 }
